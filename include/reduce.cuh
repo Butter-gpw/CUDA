e752cf32e2decc14bbb29622bf6d10a51e4f8280
Butter-gpw <709749896@qq.com>
2024-08-26 13:50:29 +0000

    reduce all

--- include/reduce.cuh
+++ include/reduce.cuh
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <cfloat>
-
+#define FLOAT4(value) (reinterpret_cast<float4*>(&(value))[0])
 #define WARP_SIZE 32
 // Warp Reduce Sum
 template<const int kWarpSize = WARP_SIZE>
@@ -52,4 +52,50 @@ __device__ __forceinline__ float block_reduce_max(float val){
     val = (lane < NUM_WARPS) ? shared[lane] : -FLT_MAX;
     val = warp_reduce_max<NUM_WARPS>(val);
     return val;
+}
+
+
+template<const int NUM_THREADS=128> //把需要求和的数组分为128个数一组先求和再汇聚
+__global__ void block_all_reduce_sum(float *a, float *y, int N){
+    int tid = threadIdx.x; //块内线程号
+    int idx = blockIdx.x * NUM_THREADS  + tid;//数组中数据的序号
+    constexpr int NUM_WARPS = (NUM_THREADS + WARP_SIZE-1) / WARP_SIZE; //线程束个数
+    __shared__ float reduce_smem[NUM_WARPS]; //按线程束数量分配share memory
+    float sum = (idx <N) ? a[idx]:0.0f; //加载数据到线程上
+    int warp = tid /WARP_SIZE; //该线程属于哪个线程束
+    int lane = tid % WARP_SIZE; //该线程属于线程束的哪个线程
+
+    sum = warp_reduce_sum<WARP_SIZE>(sum);//同一个线程束内的数据先计算一个sum
+    //把每个线程束的sum存到share memory中
+    if(lane==0) reduce_smem[warp] = sum;
+
+    __syncthreads();
+
+    sum = (lane < NUM_WARPS) ? reduce_smem[lane] : 0.0f;
+    if(warp==0) sum = warp_reduce_sum<WARP_SIZE>(sum); //在第一个线程束中继续计算sum
+    if(tid==0) atomicAdd(y, sum);//第一个线程把该数据块的sum加到y上
+}
+
+template<const int NUM_THREADS=128/4>//block的数量是一样的，但是每个block的线程束少了
+__global__ void block_all_reduce_sum_vec4(float *a, float *y, int N){//float4优化
+    int tid = threadIdx.x;
+    int idx = (blockIdx.x * NUM_THREADS + tid)*4;//数组中数据的序号
+    constexpr int NUM_WARPS = (NUM_THREADS + WARP_SIZE-1) / WARP_SIZE; //线程束个数
+    __shared__ float reduce_smem[NUM_WARPS]; //按线程束数量分配share memory
+
+    float4 reg_a = FLOAT4(a[idx]);
+    float sum = (idx < N) ? (reg_a.x+ reg_a.w+reg_a.y+reg_a.z) :0.0f;
+
+    int warp = tid /WARP_SIZE; //该线程属于哪个线程束
+    int lane = tid % WARP_SIZE; //该线程属于线程束的哪个线程
+
+    sum = warp_reduce_sum<WARP_SIZE>(sum);//同一个线程束内的数据先计算一个sum
+    //把每个线程束的sum存到share memory中
+    if(lane==0) reduce_smem[warp] = sum;
+
+    __syncthreads();
+
+    sum = (lane < NUM_WARPS) ? reduce_smem[lane] : 0.0f;
+    if(warp==0) sum = warp_reduce_sum<WARP_SIZE>(sum); //在第一个线程束中继续计算sum
+    if(tid==0) atomicAdd(y, sum);//第一个线程把该数据块的sum加到y上
 }
--- main.cu
+++ main.cu
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <iostream>
 #include <sys/time.h>
 #include "gemm.cuh"
 #include "utils.cuh"
@@ -26,20 +27,28 @@ int main(void){
     for(int i=0;i<n;++i){
         host[i] = i;
     }
+    float* hostSum, *deviceSum;
+    hostSum = new float(0);
     cudaMalloc(&input, n * sizeof(float));
     cudaMalloc(&output, (n/ WARP_SIZE)* sizeof(float));
+    cudaMalloc(&deviceSum, sizeof(float ));
+    cudaMemcpy(deviceSum, hostSum,sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(input, host,n * sizeof(float), cudaMemcpyHostToDevice);
 
     int blockSize = 128;
     int numBlocks = (n + blockSize-1) /blockSize;
-    warp_reduce_sum_test<<<numBlocks, blockSize>>>(output, input, n);
+    block_all_reduce_sum_vec4<<<numBlocks, blockSize>>>(input, deviceSum, n);
     cudaDeviceSynchronize();
     cudaError_t err = cudaGetLastError();
     if (err != cudaSuccess) {
         printf("CUDA Error: %s\n", cudaGetErrorString(err));
     }
+    cudaMemcpy(hostSum, deviceSum, sizeof(float ), cudaMemcpyDeviceToHost);
+    printf("sum:%f\n", *hostSum);
     cudaFree(input);
     cudaFree(output);
+    cudaFree(deviceSum);
+    delete hostSum;
 
     return 0;
 }
